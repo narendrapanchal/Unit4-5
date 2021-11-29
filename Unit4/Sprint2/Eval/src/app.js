1340737eecbf2d9b4a5d390d8b233a787d114652
narendra panchal <narendrapanchal020@gmail.com>
2021-11-29 14:35:21 +0530

    Schema

--- Unit4/Sprint2/Eval/src/app.js
+++ Unit4/Sprint2/Eval/src/app.js
@@ -1,1 +1,19 @@
 const express=require("express");
+const connect =require("./configs/db");
+const app=express();
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+app.listen(12345,async()=>{
+    await connect();
+    console.log(1234);
+})
---
+++ Unit4/Sprint2/Eval/src/model/company.model.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+const companySchema=mongoose.Schema({
+    company_name:{type:String,required:true,unique:true},
+    company_datails:{type:String, required:true},
+    job_ids:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"job",
+        required:true,
+    }]
+})
+module.exports= mongoose.model("company",companySchema);
