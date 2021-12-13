249cac63b622c4555caa65ec80da8fbf2cf9c56c
narendra panchal <narendrapanchal020@gmail.com>
2021-12-13 17:00:33 +0530

    Done

--- Unit4/Coding/src/controllers/movie.controller.js
+++ Unit4/Coding/src/controllers/movie.controller.js
@@ -26,5 +26,27 @@ router.get=("/",authenticate,async(req,res)=>{
         res.status(500).send({error:e});
     }
 })
+router.get=("/actor",authenticate,async(req,res)=>{
+    try{
+        const user=req.user;
+        const posts=await Movie.find().lean().exec();
+        let obj=[];
+        posts.forEach((ele)=>{
+            let arr=ele.actors;
+            let check=false;
+            arr.forEach((eleact)=>{
+                if(eleact==req.body.actor)
+                check=true;
+            })
+            if(check)
+            obj.push(ele);
+
+        })
+        res.send(obj);
+    }catch(e){
+        res.status(500).send({error:e});
+    }
+})
+
 
 module.exports=router;
--- Unit4/Coding/src/controllers/show.controller.js
+++ Unit4/Coding/src/controllers/show.controller.js
@@ -1,7 +1,35 @@
 const express= require("express");
 let router=express.Router();
 let authenticate=require("../middlewares/authenticate");
+let Movie=require("../models/movie.model");
 let Show=require("../models/show.model");
+
+router.post=("/location/",authenticate,async(req,res)=>{
+    try{
+        const user=req.user;
+        const movies=await Movie.find().lean().exec();
+        const shows=await Show.findById(req.body.id).lean().exec();
+        let nearestmovie=[];
+        movies.forEach((ele)=>{
+            if(ele.location==req.body.location)
+            nearestmovie.push(ele);
+        })
+        let desire=[];
+        nearestmovie.forEach((ele)=>{
+            shows.forEach((showele)=>{
+                if(ele._id.toString()==showele.movie_Id.toString()){
+                    if(showele.total_seats>=req.body.seat)
+                    desire.push(showele);
+                }
+            })
+        })
+        res.send(desire);
+    }catch(e){
+        res.status(500).send({error:e});
+    }
+})
+
+
 router.post=("/",authenticate,async(req,res)=>{
     try{
         const user=req.user;
@@ -43,6 +71,21 @@ router.get=("/get_seats",authenticate,async(req,res)=>{
 
         const posts=await Show.findById(req.body.id).lean().exec();
         res.send(posts.total_seats);
+    }catch(e){
+        res.status(500).send({error:e});
+    }
+});
+
+
+
+router.post=("/seat",authenticate,async(req,res)=>{
+    try{
+        const user=req.user;
+        const posts=await Show.findById(req.body.id).lean().exec();
+        if(req.body.seat<=posts.total_seats){
+            res.status(200).send(posts);
+        }else
+        res.status(200).send("Not enough seats");
     }catch(e){
         res.status(500).send({error:e});
     }
