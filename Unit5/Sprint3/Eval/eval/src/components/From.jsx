bdd835e25b2ac51279811e42cc42e54e39d4a47f
narendra panchal <narendrapanchal020@gmail.com>
2022-01-03 14:55:43 +0530

    First

---
+++ Unit5/Sprint3/Eval/eval/src/components/From.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export const Form =(getData)=>{
+    const [data,setData]=useState({
+        Title:"",
+        Ingrediants:"",
+        Time:"",
+        Image:"",
+        Instructions:""
+    })
+   const handleChange=(e)=>{
+       let [name,value]=e.target;
+       setData({...data,[name]:value});
+   } 
+   const handleData=()=>{
+       getData(data);
+   }
+    return (
+        <div>
+            <form action="" onSubmit={handleData}>
+            <input type="text" name="Title" placeholder="Title"onChange={handleChange} />
+            <input type="text" name="Ingrediants" placeholder="Ingrediants" onChange={handleChange}  />
+            <input type="number" name="Time" placeholder="Time to coock" onChange={handleChange}  />
+            <input type="text" name="Image" placeholder="Image Link" onChange={handleChange} />
+            <input type="text" name="Instructions" placeholder="Instaruction" onChange={handleChange} />
+            <input type="submit" >Submit</input>
+            </form>
+        </div>
+    )
+}
