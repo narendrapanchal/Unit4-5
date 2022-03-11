9932e681e6e059015090be6ac897d22b47840f96
narendra panchal <narendrapanchal020@gmail.com>
2022-03-11 23:29:13 +0530

    done

--- Unit5/u6sp4/fullstack/src/You/index.js
+++ Unit5/u6sp4/fullstack/src/You/index.js
@@ -3,9 +3,9 @@ const app=express();
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
-const crudTeacher=require("./controller/teacher.controller");
-const crudClass=require("./controller/class.controller");
-const  connect  = require("./configs/db");
+const crudTeacher=require("./src/controller/teacher.controller");
+const crudClass=require("./src/controller/class.controller");
+const  connect  = require("./src/configs/db");
 app.use("/teachers",crudTeacher);
 app.use("/classes",crudClass);
 let port = process.env.PORT || 4444
